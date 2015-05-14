78ceaef989f842fc47650afd2e2d6ef3759d175a
Ryan Bahniuk <ryan@wealthfronts-MacBook-Pro.local>
2015-05-14 15:47:47 -0700

    upticking postcss version

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = postcss.plugin('postcss-rails-asset-urls', function (opts) {
   opts = opts || {};
 
   var replacements = new RegExp('url', 'gi');
-
+  
   return function (css) {
     css.nodes.forEach(function(node) {
       node.nodes.forEach(function(node) {
