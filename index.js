f93bb64fe7dec1859f36aa78143d20a2c5f641d5
Ryan Bahniuk <ryan@wealthfronts-MacBook-Pro.local>
2015-05-15 07:58:10 -0700

    improving replacement regex

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var postcss = require('postcss');
 
-var RailsAssetUrls = postcss.plugin('postcss-rails-asset-urls', function (opts) {
-  opts = opts || {};
+var replacements = new RegExp('url\(([^)]+)\)', 'gi');
 
-  var replacements = new RegExp('url', 'gi');
+function replaceMatchGroup(match, matchGroup) {
+  return "asset-url" + matchGroup;
+}
 
+var RailsAssetUrls = postcss.plugin('postcss-rails-asset-urls', function () {
   return function(css) {
     css.eachDecl(function(decl) {
-      decl.value = decl.value.replace(replacements, 'asset-url');
+      decl.value = decl.value.replace(replacements, replaceMatchGroup);
     });
   };
 });
