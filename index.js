46a51c1fd209431ada7296086c7008d8b07c85d2
Ryan Bahniuk <ryan@wealthfronts-MacBook-Pro.local>
2015-06-10 23:03:40 -0700

    adding version of sass-rails as an option

--- index.js
+++ index.js
@@ -10,25 +10,40 @@ function replaceWithImageUrl(match, matchGroup) {
   return "image-url" + matchGroup;
 }
 
+function replaceWithAssetUrl(match, matchGroup) {
+  return "asset-url" + matchGroup;
+}
+
 function isImage(string) {
   return !!string.match(/(\.png|\.jpg|\.jpeg|\.gif|\.svg|\.tif|\.tiff)/);
 }
 
-var RailsAssetUrls = postcss.plugin('postcss-rails-asset-urls', function () {
-  return function(css) {
-    css.eachDecl(function(decl) {
-      if (decl.parent.name === 'font-face') {
-        decl.value = decl.value.replace(replacements, replaceWithFontUrl);
-      } else if (isImage(decl.value)) {
-        decl.value = decl.value.replace(replacements, replaceWithImageUrl);
-      }
-    });
-  };
+var RailsAssetUrls = postcss.plugin('postcss-rails-asset-urls', function (version) {
+  version = version || '4.0.0';
+
+  if (version.substring(0, 1) > 3) {
+    return function(css) {
+      css.eachDecl(function(decl) {
+        decl.value = decl.value.replace(replacements, replaceWithAssetUrl);
+      });
+    };
+  } else {
+    return function(css) {
+      css.eachDecl(function(decl) {
+        if (decl.parent.name === 'font-face') {
+          decl.value = decl.value.replace(replacements, replaceWithFontUrl);
+        } else if (isImage(decl.value)) {
+          decl.value = decl.value.replace(replacements, replaceWithImageUrl);
+        }
+      });
+    };
+  }
 });
 
 if (process.env.NODE_ENV === 'test') {
   RailsAssetUrls.replaceWithFontUrl = replaceWithFontUrl;
   RailsAssetUrls.replaceWithImageUrl = replaceWithImageUrl;
+  RailsAssetUrls.replaceWithAssetUrl = replaceWithAssetUrl;
   RailsAssetUrls.isImage = isImage;
 }
 
