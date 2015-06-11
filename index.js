385d5ad5e7b27ba17d94325a890bb0b7aedf4f73
Ryan Bahniuk <ryan@wealthfronts-MacBook-Pro.local>
2015-06-10 22:24:56 -0700

    updating file to use private methods and give image-url and font-url

--- index.js
+++ index.js
@@ -2,16 +2,34 @@ var postcss = require('postcss');
 
 var replacements = new RegExp('url\(([^)]+)\)', 'gi');
 
-function replaceMatchGroup(match, matchGroup) {
-  return "asset-url" + matchGroup;
+function replaceWithFontUrl(match, matchGroup) {
+  return "font-url" + matchGroup;
+}
+
+function replaceWithImageUrl(match, matchGroup) {
+  return "image-url" + matchGroup;
+}
+
+function isImage(string) {
+  return !!string.match(/(\.png|\.jpg|\.jpeg|\.gif|\.svg|\.tif|\.tiff)/);
 }
 
 var RailsAssetUrls = postcss.plugin('postcss-rails-asset-urls', function () {
   return function(css) {
     css.eachDecl(function(decl) {
-      decl.value = decl.value.replace(replacements, replaceMatchGroup);
+      if (decl.parent.name === 'font-face') {
+        decl.value = decl.value.replace(replacements, replaceWithFontUrl);
+      } else if (isImage(decl.value)) {
+        decl.value = decl.value.replace(replacements, replaceWithImageUrl);
+      }
     });
   };
 });
+
+if (process.env.NODE_ENV === 'test') {
+  RailsAssetUrls.replaceWithFontUrl = replaceWithFontUrl;
+  RailsAssetUrls.replaceWithImageUrl = replaceWithImageUrl;
+  RailsAssetUrls.isImage = isImage;
+}
 
 module.exports = RailsAssetUrls;
